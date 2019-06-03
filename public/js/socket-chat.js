144f207a1fabd84cf5f5c6bf592a3dff9575d646
rsoriano <rsorianosanchis@gmail.com>
2019-06-03 23:40:17 +0200

    måndag 03/06/2019 -- 23:39 -- bugg i håller på socket usr/kommunikation

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,22 +1,29 @@
 let socket = io();
-
+/*************************************************************/
+//man tar namn från url, till exempe
+//http://localhost:3000/chat.html?name=Lola
+//
 let params = new URLSearchParams(window.location.search);
+//
 if (!params.has('name')) {
     window.location = 'index.html';
-    throw new Error('Users name is necessary')
-
+    throw new Error('Namn är et krav')
 }
+//
 let user = {
-    name: params.get('name')
-}
-
+        name: params.get('name')
+    }
+    /************************************************************/
 
 socket.on('connect', function() {
     console.log('Ansluted till Server');
-    socket.emit('loginChat', name)
+    socket.emit('loginChat', user, function(resp) {
+        console.log('Online Users', resp);
+
+    })
 });
 
-// Höra
+// koppla av
 socket.on('disconnect', function() {
 
     console.log('Man har förlorat servers anslutning');
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,45 +1,20 @@
 const { io } = require('../server');
+const { Users } = require('../classes/users.js');
+
+const users = new Users();
 
 
 io.on('connection', (client) => {
 
     console.log('User is connected');
-    client.on('loginChat', (user) => {
-        console.log(user);
+
+    client.on('loginChat', (data, cb) => {
+        if (!data.name) {
+            return cb({ error: true, msg: 'the name is required' });
+        }
+        let members = users.addUser(client.id, data.name);
+        cb(members)
     })
 
-    // client.emit('sendMessage', {
-    //     user: 'Admin',
-    //     msg: 'Välkomen till chat'
-    // });
-
-
-
-    // client.on('disconnect', () => {
-    //     console.log('User disconnected');
-    // });
-
-    // // Höra klienten
-    // client.on('sendMessage', (data, callback) => {
-
-    //     console.log(data);
-
-    //     client.broadcast.emit('sendMessage', data);
-
-
-    //     // if (mensaje.usuario) {
-    //     //     callback({
-    //     //         resp: 'TODO SALIO BIEN!'
-    //     //     });
-
-    //     // } else {
-    //     //     callback({
-    //     //         resp: 'TODO SALIO MAL!!!!!!!!'
-    //     //     });
-    //     // }
-
-
-
-    // });
 
 });
