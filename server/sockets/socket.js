b8956e4b82a4e3a4103ef7c276ebadbb110217dc
rsoriano <rsorianosanchis@gmail.com>
2019-06-04 10:46:45 +0200

    tisdag 04/06/2019 -- 10:46 -- leter efter losning,för flera instancer på samman user när updatera sin sidan

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -3,7 +3,7 @@ const { Users } = require('../classes/users.js');
 
 const users = new Users();
 
-
+// måste fixa och undvika koppla flrra gånger när user updatera sidan
 io.on('connection', (client) => {
 
     console.log('User is connected');
@@ -12,6 +12,7 @@ io.on('connection', (client) => {
         if (!data.name) {
             return cb({ error: true, msg: 'the name is required' });
         }
+        // om det finns ett ny namn returnerar i cb alla members
         let members = users.addUser(client.id, data.name);
         cb(members)
     })
