d7da7e06d6f25fae0cad56a3ff90e2b24a027378
rsoriano <rsorianosanchis@gmail.com>
2019-06-06 23:14:47 +0200

    torsdag 06/06/2019 --23:14-- online users render, få socket id med click

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,19 +3,38 @@
 //
 let vOnlineUsersList = document.getElementById('onlineUsersList');
 
-// funktioner för renderar users
+
+
+// funktioner för renderar users och få id igenom click
 function renderUsersOnline(members) { //[]
-    vOnlineUsersList.innerHTML = '';
+    //vOnlineUsersList.innerHTML = '';
     console.log(members);
     let html = '';
     html += '<li>';
-    html += `<span> Rummet Juegos ${params.get('rummet')} </span>`;
+    html += `<span><strong> Rummet: ${(params.get('rummet')!==''
+        ?params.get('rummet')
+        :'Generellt')}</strong> </span>`;
     html += '</li>';
     //
     for (let i = 0; i < members.length; i++) {
         html += `<li>`;
-        html += `<span>${members[i].name}</span >`;
+        html += `<a data-id="${ members[i].id }"
+        ><span>${members[i].name}</span ></a>`;
         html += `</li>`;
     }
     vOnlineUsersList.innerHTML = html;
+    //
+    let vUsersLinks = document.querySelectorAll('#onlineUsersList a');
+    console.log(vUsersLinks.length);
+
+    for (let i = 0; i < vUsersLinks.length; i++) {
+        vUsersLinks[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            let socketId = vUsersLinks[i].getAttribute('data-id');
+            console.log(socketId);
+            e.stopPropagation()
+        })
+    }
 }
+
+//Listeners
